cbc2b1941392891b09c38280a4d394a069821c7e
North-West-Wind <wsyn148@gmail.com>
2025-04-20 12:52:21 +0800

    pedalphile and source watcher

---
+++ waybar/scripts/source-watcher.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh 
+
+source=$1
+finish=0
+
+trap 'finish=1' SIGINT
+trap 'finish=1' SIGQUIT
+trap 'finish=1' SIGTSTP
+
+pactllog=$(mktemp)
+> $pactllog
+script -q -c "pactl subscribe" > $pactllog &
+pactlpid=$!
+
+while (( $finish != 1 ))
+do
+	first=1
+	mute=$(pactl get-source-mute "$source" | cut -c 7-)
+	if [ "$mute" = "no" ]; then
+		echo "{\"text\":\"$source\",\"class\":\"unmute\"}"
+	else
+		echo "{\"text\":\"$source\",\"class\":\"mute\"}"
+	fi
+	tail -f -n0 $pactllog | grep -qe "change"
+	> $pactllog
+done
+
+rm $pactllog
