d6ca8226e5b7f6e692e1d437bc0c11247281fcec
North-West-Wind <wsyn148@gmail.com>
2025-09-20 15:56:40 +0800

    more hko

---
+++ niri/hko/example.env.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+# This file will be sourced by the update.sh script
+# These are variables that determine which weather station's data to get
+# Modify this file and rename it to env.sh under the same directory
+# It's gitignored to avoid doxxing myself
+
+export HKO_RAINFALL_PLACE="Central & Western District"
+export HKO_TEMPERATURE_PLACE="Hong Kong Observatory"
--- niri/hko/update.sh
+++ niri/hko/update.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+source ~/.config/niri/hko/env.sh
+
 finish=0
 trap 'finish=1' SIGINT
 trap 'finish=1' SIGQUIT
@@ -11,11 +13,33 @@ mkdir -p $hko_dir
 getdata() {
 	# cleanup
 	rm -f $hko_dir/warning_*.png
+	rm -f $hko_dir/weather_info.txt
 
-	icon=$(curl -s "https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=rhrread" | jq -r '.icon[0]')
+	# weather text
+	weather_data=$(curl -s "https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=rhrread")
+	temperature=$(echo "$weather_data" | jq -r ".temperature.data[] | select(.place == \"$HKO_TEMPERATURE_PLACE\")")
+	if [ -n "$temperature" ]; then
+		echo "$temperature" | jq -jr '.value' >> $hko_dir/weather_info.txt
+		echo "°C" >> $hko_dir/weather_info.txt
+	else
+		echo "" >> $hko_dir/weather_info.txt
+	fi
+	echo "$weather_data" | jq -jr '.humidity.data[0].value' >> $hko_dir/weather_info.txt
+	echo "%" >> $hko_dir/weather_info.txt
+	rainfall=$(echo "$weather_data" | jq -r ".rainfall.data[] | select(.place == \"$HKO_RAINFALL_PLACE\")")
+	if [ -n "$rainfall" ]; then
+		echo "$rainfall" | jq -jr '.max' >> $hko_dir/weather_info.txt
+		echo "mm" >> $hko_dir/weather_info.txt
+	else 
+		echo "" >> $hko_dir/weather_info.txt
+	fi
+
+	# weather icon
+	icon=$(echo $weather_data | jq -r '.icon[0]')
 	icon_url="https://www.hko.gov.hk/images/HKOWxIconOutline/pic$icon.png"
 	curl -o "$hko_dir/current_weather.png" "$icon_url"
 
+	# weather warning icon
 	count=0
 	warnings=$(curl -s "https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=warnsum" | jq -r '.[].code' | sort)
 	for warning in ${warnings}
@@ -31,13 +55,12 @@ getdata() {
 		magick -size 50x50 xc:transparent "$hko_dir/warnings.png"
 		rm -f "$hko_dir/blank.png"
 	else
-		magick montage $hko_dir/warning_*.png -tile 3x4 -geometry 100x100+10+10 -shadow -background none "$hko_dir/warnings.png"
+		magick montage $hko_dir/warning_*.png -tile 3x4 -geometry 80x80+8+8 -shadow -background none "$hko_dir/warnings.png"
 	fi
 }
 
-getdata
-#while (( $finish != 1 ))
-#do
-#	getdata
-#	sleep 600
-#done
+while (( $finish != 1 ))
+do
+	getdata
+	sleep 600
+done
